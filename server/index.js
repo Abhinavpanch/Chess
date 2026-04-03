125b31e2aa8ff9b97937d1af31ed73dd2d95c064
Abhinavpanch <absinghpanchal.01@gmail.com>
2026-04-03 10:58:18 +0530

    remove static file serving

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const {
   createInitialState,
